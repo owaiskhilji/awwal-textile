b71b0e3dea6443bf648812f75fc7dd2f3e8b65b8
owaiskhilji <owaiskhilji6@gmail.com>
2025-08-21 04:55:31 +0500

    Remove unused import for HeroSection in HouseHold component

--- src/Pages/HouseHold.jsx
+++ src/Pages/HouseHold.jsx
@@ -1,8 +1,6 @@
-// src/pages/main.jsx
 import React, { useEffect } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import HeroSection from '../components/HouseHold/WoomenHeroSection';
 import DynamicCard from '../Components/HouseHold/HouseHoldDynamicCard';
 
 function HouseHold() {
